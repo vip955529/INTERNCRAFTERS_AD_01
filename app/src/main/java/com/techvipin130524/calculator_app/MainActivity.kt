b0e7103b1e83faf99f210e2c5db5b20926eae9d4
vip955529 <vipinvns3524@gmail.com>
2024-09-28 19:27:45 +0530

    mod icons

--- app/src/main/java/com/techvipin130524/calculator_app/MainActivity.kt
+++ app/src/main/java/com/techvipin130524/calculator_app/MainActivity.kt
@@ -24,7 +24,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        //Status bar color
         window.statusBarColor = ContextCompat.getColor(this, R.color.statusColor)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
